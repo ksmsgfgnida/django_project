ecc1970a7733cf7e07185273fd8119618228ae8a
ksmsgfgnida <elka.koz6@gmail.com>
2024-10-23 12:20:27 +0300

    work

--- app/orders/admin.py
+++ app/orders/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from orders.models import Order, OrderItem
 
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
 
 class OrderItemTabulareAdmin(admin.TabularInline):
     model = OrderItem
